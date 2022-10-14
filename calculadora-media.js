5a821535b91631fcb68f4019a485d4f497bd5327
lissmirella <mirellabagdadi@gmail.com>
2022-10-14 16:09:38 -0300

    iniciando calculadora js

---
+++ calculadora-media.js
@@ -0,0 +1,13 @@
+
+// let nota1 = 5.5
+// let nota2 = 7
+// let nota3 = 8.3
+// let nota4 = 9.7
+
+// let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+const notas = [5.5 , 6.7, 8, 8.5]
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+console.log(media)
