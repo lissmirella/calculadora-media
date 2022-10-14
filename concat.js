7e415d9cff349dd092ae8998c051d438a44dba27
lissmirella <mirellabagdadi@gmail.com>
2022-10-14 18:15:08 -0300

    inserindo arquivo concat.js

---
+++ concat.js
@@ -0,0 +1,6 @@
+const modelosMulheres = [ ` Ana`, ` Maria`, ` Rebeca`]
+const modelosHomens = [` Marcel`, ` Felipe`, ` Caio`]
+
+const totalDeModelos = modelosHomens.concat(modelosMulheres)
+
+console.log(`A lista completa de modelos selecionados é: ${totalDeModelos}.`)
