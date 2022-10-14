82075474ef213d51289b61415e91810ec61c2065
lissmirella <mirellabagdadi@gmail.com>
2022-10-14 17:47:07 -0300

    correção: é slice e não splice

--- slice.js
+++ slice.js
@@ -1,7 +1,7 @@
 const alunos = [`Ana`, `Maria`, `João`, `José`, `Ravi`, `Rachel`, `Lucas`, `Enzo`]
 console.log(`O numero total de alunos na escola é ${alunos.length}.`)
 
-const sala1 = alunos.splice(0, alunos.length/2)
-const sala2 = alunos.splice(alunos.length/2)
+const sala1 = alunos.slice(0, alunos.length/2)
+const sala2 = alunos.slice(alunos.length/2)
 
 console.log(`Sendo ${sala1.length} na sala A, e ${sala2.length} na sala B`)
