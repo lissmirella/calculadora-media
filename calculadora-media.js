5f65a3866ebe56bc94fb0e66b452b48bac364279
lissmirella <mirellabagdadi@gmail.com>
2022-10-14 16:54:22 -0300

    incluindo primeiro aqruivo js

--- calculadora-media.js
+++ calculadora-media.js
@@ -7,7 +7,13 @@
 // let media = (nota1 + nota2 + nota3 + nota4) / 4
 
 const notas = [5.5 , 6.7, 8, 8.5]
+//notas.push(8)
+notas.pop()
 
-const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+const media = (notas[0] + notas[1] + notas[2]) / notas.length
+//console.log(notas)
 
 console.log(media)
+
+console.log(`Sua média final é ${media}.`)
