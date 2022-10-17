5d9238b0334816fef035ccb15df47ba6a7a3d96f
lissmirella <mirellabagdadi@gmail.com>
2022-10-17 09:55:44 -0300

    for each

---
+++ forEach.js
@@ -0,0 +1,13 @@
+// forEach é uma função callback, ou seja, uma função que chama outra função.
+
+const notas = [ 5, 6.5, 8.5, 9]
+
+somaDasNotas = 0
+
+notas.forEach( nota => {
+    somaDasNotas += nota
+})
+
+let media =somaDasNotas/notas.length
+
+console.log(media)
