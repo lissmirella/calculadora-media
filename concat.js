6c6e7ecaf5783c847e47969b981e057a02f9fbe6
lissmirella <mirellabagdadi@gmail.com>
2022-10-17 08:07:43 -0300

    inserido lista de 2 dimensões

--- concat.js
+++ concat.js
@@ -1,3 +1,5 @@
+// CONCATENAÇÃO junta listas
+
 const modelosMulheres = [ ` Ana`, ` Maria`, ` Rebeca`]
 const modelosHomens = [` Marcel`, ` Felipe`, ` Caio`]
 
---
+++ lista2dimensoes.js
@@ -0,0 +1,7 @@
+
+const alunos = [ `João`, ` Maria`, ` Marcio`]
+const mediaDosAlunos = [ 10, 8, 7]
+
+const listaDeMediaEAlunos = [ alunos, mediaDosAlunos]
+
+console.log(`${listaDeMediaEAlunos[0][0]}, sua média final é ${listaDeMediaEAlunos[1][0]}.`)
