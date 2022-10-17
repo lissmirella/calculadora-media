e86713fc302e9dd582ff3ff9bf1d060e5f253d1c
lissmirella <mirellabagdadi@gmail.com>
2022-10-17 15:42:51 -0300

    continuação maps em map.js

--- map.js
+++ map.js
@@ -7,8 +7,22 @@ const notas = [10, 9, 8, 7 , 6]
 
 const notasAtualizadas = notas.map( notas => notas == 10 ? notas : ++notas)
 
-console.log(notasAtualizadas)
+console.log(notasAtualizadas) 
+console.log(" Acima é a lista de notas atualizadas.")
 
 //map me etrega uma nova lista
 //é um callback
 //usei ordenador ternário
+
+//USAR MAPS E MULTIPLICAR POR 10, usando uma funçã externa ao map. exibir noova lçista multiplicada.
+
+const numeros = [10,20,30,40,50]
+
+const numerosMultiplicados = numeros.map(multiplicaNumeros)
+
+function multiplicaNumeros(num) {
+    return num * 10
+}
+
+console.log(" Abaixo é a lista de numeros multiplicados.")
+console.log(numerosMultiplicados)
